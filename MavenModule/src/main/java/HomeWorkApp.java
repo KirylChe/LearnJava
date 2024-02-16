25a522b5becaf1b96dd477bebe1a885e2f1074be
cheip <cheipeshkiryl@gmail.com>
2024-02-16 12:12:19 +0100

    My first commit

--- MavenModule/src/main/java/HomeWorkApp.java
+++ MavenModule/src/main/java/HomeWorkApp.java
@@ -1,11 +1,16 @@
-public class HomeWorkApp {
-    public static void main(String[] args) {
-        printThreeWords();
+public static void main(String[] args) {
+        checkSumSign();
     }
 
-    public static void printThreeWords() {
-        System.out.println("Orange");
-        System.out.println("Banana");
-        System.out.println("Apple");
+    public static void checkSumSign() {
+        int a = 50;
+        int b = -135;
+        int sum = a + b;
+        if (sum >= 0) {
+            System.out.println("Сумма положительная");
+        }
+           else {
+            System.out.println("Сумма отрицательная");
+        }
     }
-}
+
